7912cd8ab0889ac8f47cecbe6e1931ab0414af29
Douglas Jorge Vizzacchi <douglas@vizzacchi.com.br>
2022-06-11 17:36:25 -0300

    Começando Gastronomia, e incluindo acessibilidade

--- js/accessibility-dark-mode.js
+++ js/accessibility-dark-mode.js
@@ -3,3 +3,20 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark-mode')
 })
+
+function aumenta(){
+    let el = document.querySelector('html');
+    // alterando uma propriedade
+    el.style.fontSize = '20px';
+}
+function diminui(){
+    let el = document.querySelector('html');
+    // alterando uma propriedade
+    el.style.fontSize = '15px';
+}
+function dislexia(){
+    let el = document.querySelector('html');
+    // alterando uma propriedade
+    el.style.fontFamily  = 'Dislexia';
+    el.style.fontSize    = '15px';
+}

a8803af53060fc1b7649a529a2f9e58fd274a13e
Ernesto de Oliveira <ernestodeoliveira@gmail.com>
2022-09-02 21:08:08 -0300

    Adicionada API de temperatura

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+let urlWeather = "https://api.hgbrasil.com/weather?key=ad166b98&city_name=São Paulo,SP&user_ip=remote&format=json-cors"; //URL da API
+let requestWeather = new XMLHttpRequest(); //Instancia do XMLHttpRequest
+requestWeather.open('GET', urlWeather); //Solicitacao de consulta na API
+
+//Essa funcao faz o request dos dados da API logo quando o codigo (ou seja, a pagina em si) é carregada.
+requestWeather.onload = function() {
+
+    if (requestWeather.readyState == 4 && requestWeather.status == 200) {
+        var resposta = JSON.parse(requestWeather.responseText);
+
+        var valores = resposta.results; 
+
+        var description = document.getElementById("description");
+
+        description.innerHTML = valores.temp + " <i class='bi bi-cloud-sun text-warning'></i>";
+    }     
+};
+
+//Essa funcao, avisa caso ocorra algum erro nessa consulta, e printa no console o erro propriamente dito    
+requestWeather.onerror = function() {
+    console.log("Erro:" + requestWeather);
+};
+    
+requestWeather.send();
